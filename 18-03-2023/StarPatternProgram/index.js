e8c39e51f136468d6e7448963fe9d3215130fe8a
Srimanta Pal <srimantapal205@hotmail.com>
2023-03-19 20:23:40 +0530

    Square Pattern

--- 18-03-2023/StarPatternProgram/index.js
+++ 18-03-2023/StarPatternProgram/index.js
@@ -1,6 +1,7 @@
 document.writeln(`<h2>Square Star Pattern in Javascrip</h2>`);
 let n = 5;
 let space = "";
+let space2 = "";
 let str = "*";
 
 for(let i = 0; i < n; i++){
@@ -14,3 +15,22 @@ document.writeln(space);
 //console.log(space)
 
 document.writeln(`<h2>Hollow Square Pattern</h2>`);
+for(let i = 0; i < n; i++ ){
+    for(let j = 0; j < n; j++ ){
+        if (i === 0 || i === n - 1) {
+            space2 += "*";
+        } else {
+            if (j === 0 || j === n - 1) {
+                space2 += "*"
+            } else {
+                space2 += "&nbsp; ";
+                //space2 += " ";
+            }
+        }
+    }
+    space2 += `</br>`;
+    //space2 += `\n`;
+}
+
+document.writeln(space2);
+//console.log(space)
