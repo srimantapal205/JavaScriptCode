50fec231d460912a86be37fdfeecf6d270c30835
Srimanta Pal <srimantapal205@hotmail.com>
2023-03-21 10:48:56 +0530

    Callback in javascript

--- AsynchronousJavaScript/index.js
+++ AsynchronousJavaScript/index.js
@@ -1,4 +1,4 @@
-document.writeln(`<h1>Example of an asynchronous function,</h1>`);
+//document.writeln(`<h1>Example of an asynchronous function,</h1>`);
 
 
 function print1() {
@@ -14,19 +14,53 @@ function print2() {
     console.log(number2);
 }
 
+// function print3() {
+//     const fs = require('fs');
+//     fs.readFile('./number3.txt', 'utf-8', function (err, number3) {
+//         console.log(number3);
+//         print4();
+//     })
+// }
+
+
 function print3() {
-    const fs = require('fs');
-    fs.readFile('./number3.txt', 'utf-8', function (err, number3) {
+    const callBack = function (err, number3) {
         console.log(number3);
-    })
+        print4();
+    };
+    const fs = require('fs');
+    fs.readFile('./number3.txt', 'utf-8', callBack )
 }
 
 function print4() {
     const number4 = 4;
     console.log(number4);
 }
+
 print1();
 print2();
 print3();
-print4();
 
+
+
+function printNumber(number){
+    console.log(`------------------------------------------`)
+    console.log(number);
+}
+const numbers = [1,2,3,4,5];
+numbers.forEach(printNumber);
+
+
+function f(callback) {
+    setTimeout(() => callback(), 0 )
+}
+f(() => console.log('This is callback using settimeout method'))
+
+console.log('Hello World')
+
+function f2(callback) {
+    callback();
+}
+f2(() => console.log('This is callback without settimeout method'));
+
+console.log('hello world 2')
