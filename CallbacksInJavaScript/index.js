4fc6cbf68fdd60d53129849660464e8a7f84d998
Srimanta Pal <srimantapal205@hotmail.com>
2023-03-22 22:43:42 +0530

    code done

--- CallbacksInJavaScript/index.js
+++ CallbacksInJavaScript/index.js
@@ -1,18 +1,35 @@
  function calculateSqure(number, callback){
     setTimeout(() => {
         if (typeof number !== 'number' ) {
-            throw new Error('Argument of type number is expected')
+             callback(new Error('Argument of type number is expected'));
+             return
         }
         const result = number*number;
-        callback(result);
+        callback(null, result);
     },1000);
  }
 
- try {
-    calculateSqure('bad argume', function (result) {
-        console.log(result)
-    })
- } catch (error) {
-    console.log('Caught error: '+ String(error))
- }
+//  try {
+//     calculateSqure('bad argume', function (result) {
+//         console.log(result)
+//     })
+//  } catch (error) {
+//     console.log('Caught error: '+ String(error))
+//  }
+calculateSqure('bad argument', function(error, result){
+     if (error !== null) {
+        console.log('Caught error:' + String(error));
+        return;
+     }
+     console.log(result);
+});
+
+calculateSqure(8, function (error, result) {
+    if (error !== null) {
+        console.log('Caught error:' + String(error));
+        return;
+    }
+    console.log(result);
+})
  
+
