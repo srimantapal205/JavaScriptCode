912d69d9221660d5b216e9536bb2eed64779612c
Srimanta Pal <srimantapal205@hotmail.com>
2023-03-23 22:29:51 +0530

    update test case

--- javascriptCodetest/testing-callback/test/calculate-squre.test.js
+++ javascriptCodetest/testing-callback/test/calculate-squre.test.js
@@ -2,9 +2,19 @@ const calculateSquare = require('../calculate-square.js');
 const expect = require('chai').expect;
 
 describe('calculateSquare', function(){
-    it('should return 4 if passed 2', function(){
+    it('should return 4 if passed 2', function(done){
         calculateSquare( 2, function(error, result){
+            console.log('Callback gets called')
             expect(result).to.equal(4);
+            done();
         })
+    });
+    it('Should run an error if passed a string', function (done) {
+        calculateSquare('string', function (error, result) {
+            expect(error).to.not.equal(null);
+            expect(error.message).to.equal('Argument of type number is expected');  
+            done()
+        })
+      
     })
 })
