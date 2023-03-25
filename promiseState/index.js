192fc272154e6658231d32923ff68ee93a2689fe
Srimanta Pal <srimantapal205@hotmail.com>
2023-03-25 20:14:47 +0530

    Create Sentence first letter captial

--- promiseState/index.js
+++ promiseState/index.js
@@ -25,4 +25,21 @@ const createPromise = new Promise((resolve, reject) => {
     .then(value => {console.log(value)}, reason =>{ console.log(reason)} 
     )
  
- 
+
+
+    // Using Promise create a program  in a string first letter should be capetal
+    function capitalize(text) {
+        return text[0].toUpperCase() + text.substr(1);
+    }
+    
+    function capitalize(text) {
+        return new Promise((resolve, reject) => {
+            if (typeof text !== 'string') {
+                return reject (new Error('Argument must be a string'));
+            }
+            const textResult  = text[0].toUpperCase() + text.substring(1);
+            resolve(textResult) 
+        })
+    }
+
+    capitalize('publishing and graphic design').then(value => console.log(value), reason => console.log(reason));
