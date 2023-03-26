70bd1d4e0ffd982da86d3babcd8cb00b0c25db02
Srimanta Pal <srimantapal205@hotmail.com>
2023-03-26 19:48:05 +0530

    Javascript diffrent Control Flow  process

---
+++ ControlFlow/index.js
@@ -0,0 +1,11 @@
+//Javascript ternary operators
+
+let age =  18;
+let message ='';
+age >= 16 ? (message ="You can Derive"):(message="You can Not Drive");
+
+console.log(message);
+
+let speed = 90;
+let message2 = speed >= 120 ? 'To Fast' : speed >= 80 ? 'Fast' : 'OK';
+console.log(message2) 
