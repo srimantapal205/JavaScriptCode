e60ad55bb0502251968e28fa9c3f35112e32a42d
Srimanta Pal <srimantapal205@hotmail.com>
2023-03-28 08:59:47 +0530

    use of Switch acase and Comma operator.

--- ControlFlow/index.js
+++ ControlFlow/index.js
@@ -9,3 +9,50 @@ console.log(message);
 let speed = 90;
 let message2 = speed >= 120 ? 'To Fast' : speed >= 80 ? 'Fast' : 'OK';
 console.log(message2) 
+
+
+
+//JavaScript switch case statement
+
+let day = 3;
+let dayname;
+switch (day) {
+    case 1 :
+        dayname = 'Sunday';
+        break;
+    case 2:
+        dayname = 'Monday';
+        break;
+    case 3:
+        dayname = 'Tuesday';
+        break;
+    case 4:
+        dayname = 'Wednesday';
+        break;
+    case 5:
+        dayname = 'Thursday';
+        break;
+    case 6:
+        dayname = 'Friday';
+        break;
+    case  7:
+        dayname = 'Saturday'; 
+        break;
+    default:
+       dayname = 'Invalid Day';
+}
+
+document.writeln(`<p>Today is :${dayname}</p>`);
+
+//the JavaScript comma operator
+
+let board = [1,2,3,4,5,6,7,8,9];
+let s = "";
+for (let i = 0, j = 1; i < board.length; i++, j++) {
+    s += board[i] + ' ';
+    if (j%3===0) {
+        console.log(s);
+        document.write(`<span>${s}</span><br>`)
+        s = ' ';
+    }
+}
