d5d5047c537b79cb4c60a38e66d8661a8618f9e8
Srimanta Pal <srimantapal205@hotmail.com>
2023-03-29 22:37:27 +0530

    JS First-Class Citizens example

--- javascriptFunction/index.js
+++ javascriptFunction/index.js
@@ -26,3 +26,70 @@ let result = sum2(10, 11)
 
 document.writeln(`<p>${result}</p>`);
 
+
+//Passing a function to another function
+
+function avarage(a,b,fn){
+   return fn(a,b)/2;
+}
+let result2 = avarage(10,30,sum2);
+ document.writeln(`<p>${result2}</p>`);
+
+
+
+ // Returning function from function
+
+ function compareBy(propertyName){
+   return function(a,b){
+      let x = a[propertyName],
+      y = b[propertyName];
+      if(x>y){
+         return 1
+      } else if (x<y) {
+          return -1
+      }else{
+         return 0
+      }
+   }
+ }
+ let product = [
+   {neme: 'iPhone', price:900},
+   {neme: 'Samsung Galaxy', price:850},
+   {neme: 'Sony Xperia', price:700}
+ ];
+
+ console.log('Products sorted by name');
+ product.sort(compareBy('name'))
+console.table(product)
+
+console.log('Products sorted by price');
+product.sort(compareBy('price'))
+console.table(product)
+
+
+//  JavaScript Functions are First-Class Citizens example
+
+function cmToIn(length){
+   return length/2.54;
+}
+function inToCm(length){
+   return length*2.54;
+}
+function convert(fn, length){
+   return fn(length);
+}
+ 
+let inches = convert(cmToIn, 10);
+document.writeln(`<p>${inches}inches</p>`)
+
+let cm = convert(inToCm, 10);
+document.writeln(`<p>${cm}cm</p>`)
+
+
+
+
+
+
+
+
+
