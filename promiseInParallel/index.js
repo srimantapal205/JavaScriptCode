6a55110886e6c5e009b74b4a33c9718df3bfb04c
Srimanta Pal <srimantapal205@hotmail.com>
2023-04-01 15:09:33 +0530

    using promise  allSettled

--- promiseInParallel/index.js
+++ promiseInParallel/index.js
@@ -1,18 +1,24 @@
 function askFirstDealear() {
-   return new Promise((resolve, reject) => { setTimeout(() => resolve(80000), 3000 ) } )
+   return new Promise((resolve, reject) => { setTimeout(() => resolve(80000), 1000 ) } )
 }
 
 function askSecondDealer() {
-   return new Promise((resolve, reject) => { setTimeout(() => {resolve(12000),5000 } ) } )
+   return new Promise((resolve, reject) => { setTimeout(() => {reject('Not Suitable Car'),2000 } ) } )
 }
 
 function askThirdDealer() {
-   return new Promise((resolve, reject) => { setTimeout(() => {resolve(123456789), 8000 } ) } )
+   return new Promise((resolve, reject) => { setTimeout(() => {resolve(123456789), 3000 } ) } )
 }
 
-Promise.all([askFirstDealear(), askSecondDealer(), askThirdDealer()])
-         .then(prices => {
-            console.log(prices)
-         });
+// Promise.all([
+//    askFirstDealear().catch((error) => { return error } ), 
+//    askSecondDealer().catch((error) => { return error } ), 
+//    askThirdDealer()]).catch((error) => { return error } )
+//          .then(prices => {
+//             console.log(prices)
+//          })
+//          .catch((error) => { 
+//             console.log(error)
+//           } );
       
-      
+Promise.allSettled([askFirstDealear(),askSecondDealer(),askThirdDealer()]);
